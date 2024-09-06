38874ab85e186dcfb9ba15dfaa0e6587294427af
RANIL K,R <ranilkr77@gmail.com>
2024-09-06 15:45:28 +0530

    Add files via upload

---
+++ MSQL ASSIGNMENT6.sql
@@ -0,0 +1,85 @@
+ CREATE DATABASE COUNTRY_FIELD;
+ 
+ USE COUNTRY_FIELD;
+ 
+ CREATE TABLE COUNTRY
+ (COUNTRY_ID INT PRIMARY KEY ,
+ COUNTRY_NAME VARCHAR(30) ,
+ POPULATION  DECIMAL(30), 
+ AREA VARCHAR (30)
+ );
+ 
+ SELECT * FROM COUNTRY;
+ INSERT INTO COUNTRY (COUNTRY_ID,COUNTRY_NAME,POPULATION,AREA) VALUES
+ 
+ (1 ,'India' ,500000 , 2973190),
+ (2 ,'China' ,400000 , 9388211),
+ (3 ,'USA',450000 , 9147420),
+ (4 ,'UK' ,350000 , 770880),
+ (5 ,'Nigeria' ,250000 , 910770),
+ (6 ,'Brazil' ,250000 , 8358140),
+ (7 ,'Canada' ,3550000 , 16376870),
+ (8 ,'Australia' ,450900 ,1000000),
+ (9 ,'Mexico' ,239000 , 1943950),
+ (10 ,'Philippines' ,270000 , 298170);
+ 
+ SELECT * FROM COUNTRY;
+ 
+ 
+ CREATE TABLE PERSONS (PERSON_ID INT PRIMARY KEY NOT NULL,
+ F_NAME VARCHAR (30)
+ ,L_NAME VARCHAR(30)
+ ,POPULATION DECIMAL(30)
+ ,RATING DECIMAL(3.1)
+ ,COUNTRY_ID DECIMAL (30)
+ , COUNTRY_NAME VARCHAR(30)
+ );
+ SELECT * FROM PERSONS;
+ 
+ INSERT INTO PERSONS (PERSON_ID,F_NAME,L_NAME,POPULATION,RATING,COUNTRY_ID,COUNTRY_NAME) VALUES
+ 
+ (30,'Linto' ,'Jhon' , 500000 ,2.0 , 1 ,'India'),
+ (29, 'Mirchel' ,'Khan' ,400000 ,1.0 , 2 ,null),
+ (28, 'Mubarak' ,'Sidhik',450000 ,1.6 , 3 ,'UK'),
+(27, 'Siraj' ,'Sahid' ,350000 ,2.7 , 4 , 'USA'),
+(26, 'Rhul' ,'Menon' , 250000 ,1.6 , 5 ,'Nigeria'),
+(25, 'Namitha' ,'Premodh' ,250000 ,6.5 ,6 ,'Brazil'),
+(24, 'Vidhu'  ,'Predhavan' ,355000 ,4.6 , 7 ,null),
+(23, 'Nikhil' , 'Sanker' , 450900 ,3.9 , 8 ,'Canada'),
+(22, 'Virat' ,'Kholi' , 230900 ,1.4 ,  9 ,null),
+(21, 'Rohit' ,'Sharma' ,270000 ,5.9 , 10 , 'Philippines');
+
+
+
+
+
+
+
+
+#(1)Perform inner join, Left join, and Right join on the tables?
+#INNER JOIN
+SELECT COUNTRY.COUNTRY_ID,PERSONS.COUNTRY_ID,  
+PERSONS.POPULATION,PERSONS.COUNTRY_NAME,PERSONS.
+RATING FROM PERSONS INNER JOIN COUNTRY ON PERSONS.COUNTRY_ID = COUNTRY.COUNTRY_ID;
+
+
+#LEFT JOIN
+SELECT COUNTRY.COUNTRY_ID,PERSONS.COUNTRY_ID, 
+PERSONS.POPULATION,PERSONS.COUNTRY_NAME,PERSONS.
+RATING FROM PERSONS LEFT JOIN COUNTRY ON PERSONS.COUNTRY_ID = COUNTRY.COUNTRY_ID;
+
+#RIGHT JOIN
+SELECT COUNTRY.COUNTRY_ID,PERSONS.COUNTRY_ID, 
+PERSONS.POPULATION,PERSONS.COUNTRY_NAME,PERSONS.
+RATING FROM PERSONS RIGHT JOIN COUNTRY ON PERSONS.COUNTRY_ID = COUNTRY.COUNTRY_ID;
+
+
+#(2)List all distinct country names from both the Country and Persons tables.
+select distinct COUNTRY_NAME from PERSONS union select distinct COUNTRY_NAME from COUNTRY;
+
+#(3)List all country names from both the Country and Persons tables, including duplicates
+select distinct COUNTRY_NAME from PERSONS union all select distinct COUNTRY_NAME from COUNTRY;
+
+#(4)Round the ratings of all persons to the nearest integer in the Persons table.
+select round(RATING) as Rounder_RATING from PERSONS;
+
